396cb673b27d1bac61cb95de3c99e132c073b4f9
MennaTuallahmohamed <menna11mohamed@gmail.com>
2024-09-23 13:52:23 +0300

    first commit

---
+++ lib/Screens/Signup/components/signup_form.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import '../../../constants.dart';
+import '../../Login/login_screen.dart';
+import '../../Welcome/components/already_have_an_account_acheck.dart';
+
+
+
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _SignUpFormState createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? password;
+  String? confirmPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            cursorColor: kPrimaryColor,
+            onSaved: (email) {},
+            decoration: const InputDecoration(
+              hintText: "Your email",
+              prefixIcon: Padding(
+                padding: EdgeInsets.all(defaultPadding),
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              obscureText: true,
+              textInputAction: TextInputAction.next,
+              cursorColor: kPrimaryColor,
+              onChanged: (value) {
+                password = value;
+              },
+              decoration: const InputDecoration(
+                hintText: "Your password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: defaultPadding),
+            child: TextFormField(
+              obscureText: true,
+              textInputAction: TextInputAction.done,
+              cursorColor: kPrimaryColor,
+              onChanged: (value) {
+                confirmPassword = value;
+              },
+              validator: (value) {
+                if (value != password) {
+                  return "Passwords don't match";
+                }
+                return null;
+              },
+              decoration: const InputDecoration(
+                hintText: "Confirm password",
+                prefixIcon: Padding(
+                  padding: EdgeInsets.all(defaultPadding),
+                  child: Icon(Icons.lock),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: defaultPadding / 2),
+          ElevatedButton(
+            onPressed: () {
+              if (_formKey.currentState!.validate()) {
+                // Proceed with the sign-up process if the form is valid
+              }
+            },
+            child: Text("Sign Up".toUpperCase()),
+          ),
+          const SizedBox(height: defaultPadding),
+          AlreadyHaveAnAccountCheck(
+            login: false,
+            press: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return const LoginScreen();
+                  },
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/Screens/Welcome/components/background.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Background extends StatelessWidget {
+  final Widget child;
+  const Background({
+    Key? key,
+    required this.child,
+    this.topImage = "assets/images/main_top.jpg", // Changed from .png to .jpg
+    this.bottomImage =
+        "assets/images/login_bottom.jpg", // Changed from .png to .jpg
+  }) : super(key: key);
+
+  final String topImage, bottomImage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SizedBox(
+        width: double.infinity,
+        height: MediaQuery.of(context).size.height,
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Positioned(
+              top: 0,
+              left: 0,
+              child: Image.asset(
+                topImage,
+                width: 120,
+              ),
+            ),
+            // Uncomment the following lines if you want to use the bottom image as well
+            // Positioned(
+            //   bottom: 0,
+            //   right: 0,
+            //   child: Image.asset(bottomImage, width: 120),
+            // ),
+            SafeArea(child: child),
+          ],
+        ),
+      ),
+    );
+  }
+}
